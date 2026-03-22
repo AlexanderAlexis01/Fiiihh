149812ab87a9ca812974392eb1bd226cbee5ce52
AlexanderAlexis01 <120094886+AlexanderAlexis01@users.noreply.github.com>
2026-03-22 18:41:54 +0100

    Add files via upload
    Fiiish

---
+++ script.js
@@ -0,0 +1,15 @@
+const fish = document.getElementById('fish');
+const audio = new Audio('/squeaky.mp3');
+
+fish.addEventListener('click', () => {
+  fish.classList.remove('bounce');
+  void fish.offsetWidth;
+  fish.classList.add('bounce');
+  
+  audio.currentTime = 0;
+  audio.play();
+});
+
+fish.addEventListener('animationend', () => {
+  fish.classList.remove('bounce');
+});
